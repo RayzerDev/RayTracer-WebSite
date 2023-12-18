640323d9cc1ed5667c3414e181dc717a00c100cd
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-18 11:03:42 +0100

    :zap: Page d'accueil basique avec le layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/scss/app.scss'])
-</head>
-<body>
-<div class="container py-4 px-3 mx-auto text-white">
-    <h1>Bienvenue sur la page d'accueil</h1>
-</div>
-</body>
-</html>
+<x-layout titre="Accueil">
+    <h1>Bienvenue sur la page web de la SAE 3.01</h1>
+
+    <p></p>
+
+</x-layout>
