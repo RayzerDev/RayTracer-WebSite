5f9eef3ce70be048c01757bdd6475c47a3d3a451
matteo_guiffroy <matteo_guiffroy@ens.univ-artois.fr>
2023-12-18 17:20:27 +0100

    Filtrage

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -7,9 +7,16 @@ use Illuminate\Http\Request;
 
 class SceneController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $scenes = Scene::all();
-        return view('scenes.index', compact('scenes'));
+        $equipes = Scene::distinct('equipe')->pluck('equipe')->toArray();
+
+        $selectedTeam = $request->input('equipe');
+
+        $scenes = Scene::when($selectedTeam, function ($query) use ($selectedTeam) {
+            return $query->where('equipe', $selectedTeam);
+        })->get();
+
+        return view('scenes.index', compact('scenes', 'equipes', 'selectedTeam'));
     }
 }
--- resources/views/scenes/index.blade.php
+++ resources/views/scenes/index.blade.php
@@ -2,6 +2,16 @@
 <div>
     <h1>Liste des Scènes</h1>
     <ul>
+        <form method="GET" action="{{ route('scenes.index') }}">
+            <label for="team">Choisir une Équipe:</label>
+            <select name="team" id="team">
+                <option value="">Toutes les équipes</option>
+                @foreach ($equipes as $equipe)
+                    <option value="{{ $equipe }}" {{ $selectedTeam == $equipe ? 'selected' : '' }}>{{ $equipe }}</option>
+                @endforeach
+            </select>
+            <button type="submit">Filtrer</button>
+        </form>
         @foreach ($scenes as $scene)
             <li>
                 <strong>Nom de la Scène:</strong> {{ $scene->nomScene }}<br>
