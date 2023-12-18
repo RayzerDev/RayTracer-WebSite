1b2ba7d019149f761d2ee3cfcd57b38e64fb1a31
RayzerDev <rayzergamer62@outlook.fr>
2023-12-18 23:21:40 +0100

    Ajout de la méthode pour vider les images d'upload

--- database/migrations/2023_12_18_113036_create_table_scene.php
+++ database/migrations/2023_12_18_113036_create_table_scene.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $this->flushImageScene();
         Schema::create('scenes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('userId');
