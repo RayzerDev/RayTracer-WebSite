376168b9050e59f7e99db1fa02bf5939e46f6267
Allaert Damien <damien_allaert@ens.univ-artois.fr>
2023-12-18 13:03:14 +0100

    Ajout de toute les migrations

---
+++ database/migrations/2023_12_18_113036_create_table_scene.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scenes', function (Blueprint $table) {
+            $table->id('idScene');
+            $table->string('nomScene');
+            $table->string('description');
+            $table->string('lienVignetteImage');
+            $table->string('lienExecutable');
+            $table->dateTime('dateAjout');
+            $table->string('descriptionScene');
+            $table->string('lienImage');
+            $table->int('equipe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scenes');
+    }
+};
