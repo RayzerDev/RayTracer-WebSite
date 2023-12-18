ac212e69ab3c0bc44b9694c2840282bdaeb01519
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-18 17:19:46 +0100

    :zap: Test de la responsivité

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white w-100">
     <div class="collapse navbar-collapse">
-        <div class="navbar-nav text-white mx-5">
+        <div class="navbar-nav mx-5 d-sm-flex">
+            <a class="nav-item nav-link text-white" href="/">RayTracer</a>
             <a class="nav-item nav-link text-white" href="/a-propos">A propos</a>
             <a class="nav-item nav-link text-white" href=""></a>
             <a class="nav-item nav-link text-white" href="/contact">Contact</a>
