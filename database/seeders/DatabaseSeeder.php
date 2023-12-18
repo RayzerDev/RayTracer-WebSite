a06fb32983b2f44b56e3ae5f11fc11317a3fe7d8
Allaert Damien <damien_allaert@ens.univ-artois.fr>
2023-12-18 14:46:30 +0100

    Appel de la factory de scene dans le seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Scene;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(UtilisateurSeeder::class);
         $this->call(ComSeeder::class);
+        $this->call(SceneSeeder::class);
     }
 }
