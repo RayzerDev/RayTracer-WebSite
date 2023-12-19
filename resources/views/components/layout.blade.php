21bdc72f435a9462fb911b71cf8ca3078c35f156
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-19 11:13:02 +0100

    :bug: le footer bug donc il n'est affiché que dans la section a-propos du site

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,13 +8,10 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
-<body>
+<body class="mb-5">
 <x-banner></x-banner>
 <div class="container py-4 px-3 mx-auto text-white">
     {{$slot}}
 </div>
 </body>
-<footer>
-    <x-footer></x-footer>
-</footer>
 </html>
