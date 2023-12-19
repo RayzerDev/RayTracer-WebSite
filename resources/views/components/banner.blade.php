57cd1fc5342fb39de8e70df78a29e3d7d4376ba4
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-12-19 16:32:32 +0100

    Ajout du bouton scenes

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -3,8 +3,8 @@
         <div class="navbar-nav mx-5 d-sm-flex">
             <a class="nav-item nav-link text-white" href="/">RayTracer</a>
             <a class="nav-item nav-link text-white" href="/a-propos">A propos</a>
-            <a class="nav-item nav-link text-white" href=""></a>
             <a class="nav-item nav-link text-white" href="/contact">Contact</a>
+            <a class="nav-item nav-link text-white" href="/scenes">Scenes</a>
         </div>
     </div>
     <x-header></x-header>
