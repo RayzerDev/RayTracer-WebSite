3528526c497ff401e4328cdf8249e536bef61974
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-19 11:12:18 +0100

    :bug: Bug du footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<div class="main-footer container py-4 px-3 mx-auto fixed-bottom text-white d-flex justify-content-between">
+<footer class="bottom-0 container py-4 px-3 mx-auto text-white d-flex justify-content-between mt-auto">
     Copyright &copy; {{ date('Y')}}
     <a href="http://www.iut-lens.univ-artois.fr/" target="_blank" class="text-white">
         <strong>IUT Lens - département Info.</strong>
     </a>
     <p>Par Louis, Damien, Mattéo et Anthony</p>
-</div>
+</footer>
