ba4a3be2587fac7318dbd1c754131fe940b72d84
matteo_guiffroy <matteo_guiffroy@ens.univ-artois.fr>
2023-12-19 09:46:22 +0100

    Scene par filtre d'equipe #14

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -4,19 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Scene;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
 
 class SceneController extends Controller
 {
     public function index(Request $request)
     {
-        $equipes = Scene::distinct('equipe')->pluck('equipe')->toArray();
 
-        $selectedTeam = $request->input('equipe');
-
-        $scenes = Scene::when($selectedTeam, function ($query) use ($selectedTeam) {
-            return $query->where('equipe', $selectedTeam);
-        })->get();
-
-        return view('scenes.index', compact('scenes', 'equipes', 'selectedTeam'));
+        Log::info('Hello from index');
+        $cat = $request->input('cat', null);
+        $value = $request->cookie('cat', null);
+        if (!isset($cat)) {
+            if (!isset($value)) {
+                $scenes = Scene::all();
+                $cat = 'All';
+                Cookie::expire('cat');
+            } else {
+                $scenes = Scene::where('equipe', $value)->get();
+                $cat = $value;
+                Cookie::queue('cat', $cat, 10);            }
+        } else {
+            if ($cat == 'All') {
+                $scenes = Scene::all();
+                Cookie::expire('cat');
+            } else {
+                $scenes = Scene::where('equipe', $cat)->get();
+                Cookie::queue('cat', $cat, 10);
+            }
+        }
+        $equipes = Scene::distinct('equipe')->pluck('equipe');
+        return view('scenes.index',
+            ['titre' => "Liste des scenes", 'scenes' => $scenes, 'cat' => $cat, 'equipes' => $equipes]);
     }
+
 }
--- resources/views/scenes/index.blade.php
+++ resources/views/scenes/index.blade.php
@@ -2,25 +2,30 @@
 <div>
     <h1>Liste des Scènes</h1>
     <ul>
-        <form method="GET" action="{{ route('scenes.index') }}">
-            <label for="team">Choisir une Équipe:</label>
-            <select name="team" id="team">
-                <option value="">Toutes les équipes</option>
-                @foreach ($equipes as $equipe)
-                    <option value="{{ $equipe }}" {{ $selectedTeam == $equipe ? 'selected' : '' }}>{{ $equipe }}</option>
+        <form action="{{route('scenes.index')}}" method="get">
+            <select name="cat">
+                <option value="All" @if($cat == 'All') selected @endif>-- Toutes équipes --</option>
+                @foreach($equipes as $equipe)
+                    <option value="{{$equipe}}" @if($cat == $equipe) selected @endif>{{$equipe}}</option>
                 @endforeach
             </select>
-            <button type="submit">Filtrer</button>
+            <input type="submit" value="OK">
         </form>
-        @foreach ($scenes as $scene)
-            <li>
-                <strong>Nom de la Scène:</strong> {{ $scene->nomScene }}<br>
-                <strong>Équipe:</strong> {{ $scene->equipe }}<br>
-                <strong>Date d'Ajout:</strong> {{ $scene->dateAjout }}<br>
-                <strong>Vignette:</strong> {{ $scene->lienVignetteImage }}>
-            </li>
-            <br>
-        @endforeach
+
+        <div class="scene-cards">
+            @foreach ($scenes as $scene)
+                <div class="card">
+                    <img src="{{ $scene->vignetteImage }}" alt="Vignette de la Scène">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $scene->nom }}</h5>
+                        <p class="card-text">
+                            <strong>Équipe:</strong> {{ $scene->equipe }}<br>
+                            <strong>Date d'Ajout:</strong> {{ $scene->created_at }}<br>
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </ul>
 </div>
 </x-layout>
