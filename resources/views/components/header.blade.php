1b012a28d61c5dc0da6f7b34da242c39aaeb733c
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-19 11:22:15 +0100

    :sparkles: Ajout d'une icon de compte

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,6 +6,7 @@
 @auth
     <a href="{{ route('home') }}" class="nav-item nav-link text-white d-sm-block">Bienvenue {{Auth::user()->nom}}</a>
     <a href="#" id="logout" class="nav-item nav-link text-white mx-5 d-sm-block">Logout</a>
+    <span class="material-symbols-outlined">account_circle</span>
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         {{ csrf_field() }}
     </form>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$titre}}</title>
     @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/scss/app.scss'])
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body class="mb-5">
 <x-banner></x-banner>
