6b7dc26afff832d1cbd5df30dec450e35a11c66c
RayzerDev <rayzergamer62@outlook.fr>
2023-12-19 20:40:55 +0100

    Ajout de route pour Note et Commentaire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Actions\Fortify\UpdateUserAvatar;
+use App\Http\Controllers\CommentaireController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FavoriController;
 use App\Http\Controllers\NoteController;
@@ -34,6 +35,12 @@ Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact
 Route::put('/update-avatar', [UpdateUserAvatar::class, 'update'])->name('user.update-avatar');
 
 
-Route::resource('notes', NoteController::class);
 Route::resource("/scenes", SceneController::class);
+
 Route::put('/favoris/{scene}', [FavoriController::class, 'toggle'])->name('favoris.toggle');
+
+Route::resource('commentaires', CommentaireController::class)->only(["edit", "destroy", 'update']);
+Route::post('/scenes/{scene}/commentaires', [CommentaireController::class, 'store'])->name('commentaire.store');
+
+Route::post('/scenes/{scene}/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::put("/scenes/{scene}/notes", [NoteController::class, 'update'])->name('notes.update');
